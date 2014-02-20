58917d330971b7da4226f67516bebae4e416f279
natashalysakova <natashalysakova@gmail.com>
2014-02-20 14:30:32 +0200

    начала  делать #19

---
+++ adovipavto/Help.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace adovipavto
+{
+    public partial class Help : Form
+    {
+        public Help()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(Application.StartupPath + @"\Help\index.html");
+        }
+    }
+}
